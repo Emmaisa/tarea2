57f30d9dabba86bf38800977e9e2543d4c677c12
Emmaisa <43923045+Emmaisa@users.noreply.github.com>
2020-03-28 11:14:33 -0600

    Add files via upload

---
+++ ejercicio 2/ejercicio 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			double suma = 0, total;
+			double[] num = new double[10];
+
+			for (int i = 0; i < num.Length; i++)
+			{
+				Console.WriteLine("Ingrese la nota en pocision" + i);
+				num[i] = double.Parse(Console.ReadLine());
+			}
+			suma = num();
+
+			Console.WriteLine("El total es de: ", suma);
+		}
+	}
+}
